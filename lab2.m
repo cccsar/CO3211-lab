8796c317c812e2e8daa4c2329f575f81520394d7
invitado <invitado@a13.ac.labf.usb.ve>
2018-10-17 13:35:02 -0400

    todo

---
+++ lab2.m
@@ -0,0 +1,12 @@
+# Created by Octave 4.0.3, Wed Oct 17 13:16:36 2018 EDT <invitado@a13>
+# name: A
+# type: matrix
+# rows: 5
+# columns: 5
+ 17 24 1 8 15
+ 23 5 7 14 16
+ 4 6 13 20 22
+ 10 12 19 21 3
+ 11 18 25 2 9
+
+
