0b4c9d0d616ef7e7f32899ab5be206906cf18ae3
invitado <invitado@a14.ac.labf.usb.ve>
2018-11-14 13:08:26 -0500

    nuevos cambios

--- sistemaLU.m
+++ sistemaLU.m
@@ -1,8 +1,8 @@
 function x=sistemaLU(A,b)
-  n=length(A)
-  Lu=LU(A)
-  L=tril(Lu,-1)+eye(n)
-  U=triu(Lu)
-  y=sust_adel(L,b)
-  x=sust_atras(U,y)
+  n=length(A);
+  Lu=LU(A);
+  L=tril(Lu,-1)+eye(n);
+  U=triu(Lu);
+  y=sust_adel(L,b);
+  x=sust_atras(U,y);
 endfunction
