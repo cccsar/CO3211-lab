796a7049d4bd59a61a08de255cd96ede7c845bac
invitado <invitado@a14.ac.labf.usb.ve>
2018-11-14 11:59:30 -0500

    disco de Gershgorin 2

--- disco.m
+++ disco.m
@@ -6,10 +6,10 @@ function [] =disco(A)
   r=sum(abs(A),2)-abs(diag(A)); 
   for i=1:n
     x(:,i)=r(i)*cos(circulo)+real(A(i,i));
-    y(:,i)=r(i)*sin(ciruclo)+imag(A(i,i));
+    y(:,i)=r(i)*sin(circulo)+imag(A(i,i));
   end
   e=eig(A); 
-  plot(x,y,real(e),img(e),'x');
+  plot(x,y,real(e),imag(e),'x');
 end
 
 
